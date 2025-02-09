7aaaf402dd4292e8c2d9786293d7b0f14129bb28
Denis Bekker <bekker.dev@gmail.com>
2025-02-09 16:48:47 +0500

    Abstract class Person

---
+++ Person.cs
@@ -0,0 +1,50 @@
+abstract class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string PreferredThing { get; set; }
+
+    public Person(string name, int age, string preferredThing)
+    {
+        Name = name;
+        Age = age;
+        PreferredThing = preferredThing;
+    }
+
+    public abstract void AskPreference();
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Приятно познакомиться, {Name}. Ваш возраст {Age}. {GetPreferenceDescription()}");
+    }
+
+    protected abstract string GetPreferenceDescription();
+}
+
+class MalePerson : Person
+{
+    public MalePerson(string name, int age, string carBrand)
+        : base(name, age, carBrand) {}
+
+    public override void AskPreference()
+    {
+        Console.Write("Введите вашу любимую марку машины: ");
+        PreferredThing = Console.ReadLine();
+    }
+
+    protected override string GetPreferenceDescription() => $"Любимая машина: {PreferredThing}";
+}
+
+class FemalePerson : Person
+{
+    public FemalePerson(string name, int age, string favoriteFlowers)
+        : base(name, age, favoriteFlowers) {}
+
+    public override void AskPreference()
+    {
+        Console.Write("Введите ваши любимые цветы: ");
+        PreferredThing = Console.ReadLine();
+    }
+
+    protected override string GetPreferenceDescription() => $"Любимые цветы: {PreferredThing}";
+}
--- Program.cs
+++ Program.cs
@@ -1,49 +1,36 @@
-﻿using System;
-
-class Person 
-{
-    public string Name;
-    public int Age;
-    public string Gender;
-    public string PrefferedThing;
-}
-
+﻿
 class Program
 {
     static void Main()
     {
-        Person person = new Person();
         Console.Write("Введите ваше имя: ");
-        person.Name = Console.ReadLine();
-        Console.WriteLine($"Ваше имя: {person.Name}");
+        string name = Console.ReadLine();
 
         Console.Write("Введите ваш возраст: ");
-
-        while(!int.TryParse(Console.ReadLine(), out person.Age) ||  person.Age < 0)
+        int age;
+        while (!int.TryParse(Console.ReadLine(), out age) || age < 0)
         {
             Console.WriteLine("Некорректный ввод. Введите число больше либо 0.");
-            Console.Write("Введите ваш возраст: ");
         }
-
-        Console.Write($"Ваш возраст: {person.Age}");
 
         Console.Write("Введите ваш пол (м/ж): ");
-        person.Gender = Console.ReadLine().Trim().ToLower();
+        string gender;
+        while (true)
+        {
+            gender = Console.ReadLine().Trim().ToLower();
+            if (gender == "м" || gender == "ж")
+                break;
+            Console.WriteLine("Некорректный ввод. Введите 'м' или 'ж': ");
+        }
 
-        if(person.Gender == "м")
-        {
-            Console.Write("Введите вашу любимую марку машины: ");
-            person.PrefferedThing = Console.ReadLine();
-            Console.WriteLine($"Приятно познакомиться, {person.Name}. Ваш возраст {person.Age}. Ваша любимая машина {person.PrefferedThing}");
-        }
-        else
-        {
-            Console.Write("Введите ваши любимые цветы: ");
-            person.PrefferedThing = Console.ReadLine();
-            Console.WriteLine($"Приятно познакомиться, {person.Name}. Ваш возраст {person.Age}. Ваши любимые цветы {person.PrefferedThing}");
-        }
+        Person person = gender == "м"
+            ? new MalePerson(name, age, "")
+            : new FemalePerson(name, age, "");
+
+        person.AskPreference();
+        person.DisplayInfo();
 
         Console.WriteLine("Нажмите любую клавишу для выхода...");
         Console.ReadKey();
     }
-} 
+}
